c4afad17b4ac1ccb7e902417e6a56acea85eb4bb
Fatih Yurek <fatih_yurek@emrisoft.com>
2024-01-15 16:43:43 +0300

    useFetcher hook implemented with update priority

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import CreateOrder, {
 } from "./features/order/CreateOrder";
 import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
-
+import { action as updateOrderAction } from "./features/order/UpdateOrder.jsx";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -38,6 +38,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
